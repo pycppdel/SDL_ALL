687daa77e369b5130706162c5e3ad1fd502b8040
root <pycppdel@gmail.com>
2021-10-09 15:54:32 +0200

    autosave

---
+++ SDL_ALL/SDL_ALL.h
@@ -0,0 +1,15 @@
+#include <SDL.h>
+#include <SDL_image.h>
+
+#ifdef __linux__
+#include <png.h>
+#include <zlib.h>
+#endif
+
+#ifdef _WIN32
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "SDL2main.lib")
+#pragma comment(lib, "SDL2_image.lib")
+#pragma comment(lib, "SDL2_mixer.dll")
+#undef main
+#endif
